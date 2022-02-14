e4456f8253453d9434261f67d2a8606d4c34a0f3
Aleksey Sokolov <sokolov.alexey@me.com>
2022-02-14 18:30:22 +0300

    re-wrote tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,37 +1,12 @@
 import validatePhoneNumber from '../app';
 
-test('russian number starting with 8', () => {
-  const phoneNumber = validatePhoneNumber('8 (927) 000-00-00');
-
-  expect(phoneNumber).toBe('+79270000000');
-});
-
-test('russian number starting with +7', () => {
-  const phoneNumber = validatePhoneNumber('+7 960 000 00 00');
-
-  expect(phoneNumber).toBe('+79600000000');
-});
-
-test('chineese number statring with +86', () => {
-  const phoneNumber = validatePhoneNumber('+86 000 000 0000');
-
-  expect(phoneNumber).toBe('+860000000000');
-});
-
-test('chineese number statring with 86', () => {
-  const phoneNumber = validatePhoneNumber('86 000 000 0000');
-
-  expect(phoneNumber).toBe('+860000000000');
-});
-
-test('turkish number', () => {
-  const phoneNumber = validatePhoneNumber('+90(000)-000-00-00');
-
-  expect(phoneNumber).toBe('+900000000000');
-});
-
-test('ukranian number', () => {
-  const phoneNumber = validatePhoneNumber('+380 00 0000000');
-
-  expect(phoneNumber).toBe('+380000000000');
+test.each([
+  ['8 (927) 000-00-00', '+79270000000'],
+  ['+7 960 000 00 00', '+79600000000'],
+  ['+86 000 000 0000', '+860000000000'],
+  ['86 000 000 0000', '+860000000000'],
+  ['+90(000)-000-00-00', '+900000000000'],
+  ['+380 00 0000000', '+380000000000'],
+])('testing validatePhoneNumber function', (phone, result) => {
+  expect(validatePhoneNumber(phone)).toBe(result);
 });
